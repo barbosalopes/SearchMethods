37e9cbb224de4ba74723919a23bc38553a5f24a1
1117303 <1117303@PSG10INFLB20713>
2019-02-23 09:19:26 -0300

    First commit

---
+++ Search/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    public static class BinarySearch
+    {
+
+        private static int GetMidleItemPos(int[] array)
+        {
+            return (int)Math.Truncate((double)array.Length/2);
+            
+        }
+
+        private static int[] GetNewArrayWithLowerItems(int[] array, int pos)
+        {
+            return (int[])array.Take(pos-1);
+        }
+
+        public static int Search(int[] array, int value)
+        {
+            int midleItemPos = GetMidleItemPos(array);
+            int midleItem = array[midleItemPos];
+
+            if(value == midleItemPos)
+                return array[midleItemPos];
+            else 
+                if(value < midleItem)
+                    return Search(GetNewArrayWithLowerItems(array, pos), value)
+                else 
+        }   
+    }
+}
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,14 +1,93 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Search;
+using System;
 
 namespace UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private int Value { get; set; }
+        private int ExpectedValue { get; set; }
+        private int Res { get; set; }
+        private int MidleItem { get; set; }
+        private int FirstItem { get; set; }
+        private int LastItem { get; set; }
+        private int NonExistentItem { get; set; }
+        private int ExistentItem { get; set; }
+
+
+        // Make readonly
+        private int[] Arr;
+
+        [TestInitialize]
+        public void AssemblyInit()
         {
+            Arr = new int[]{ 12, 13, 14, 20, 25, 30, 33, 50, 57, 99 };
+            FirstItem = 12;
+            LastItem = 99;
+            MidleItem = 25;
+            NonExistentItem = 34;
+            ExistentItem = 14;
+        }
+
+        [TestMethod]
+        public void TestSimple()
+        {
+            Value = ExpectedValue = 14;
+            Res = BinarySearch.Search(Arr, Value);
+            Assert.AreEqual(ExpectedValue, Res);
+        }
+
+        [TestMethod]
+        public void TestOnStart()
+        {
+            Value = ExpectedValue = FirstItem;
+            Res = BinarySearch.Search(Arr, Value);
+            Assert.AreEqual(ExpectedValue, Res);
+        }
+
+        [TestMethod]
+        public void TestOnEnding()
+        {
+            Value = ExpectedValue = LastItem;
+            Res = BinarySearch.Search(Arr, Value);
+            Assert.AreEqual(ExpectedValue, Res);
+        }
+
+        [TestMethod]
+        public void TestNonExistentObject()
+        {
+            Value = NonExistentItem;
+            Res = BinarySearch.Search(Arr, Value);
+            Assert.IsNull(Res);
+        }
+
+        [TestMethod]
+        public void TestEmptyArray()
+        {
+            Value = NonExistentItem;
+            int[] arr = new int[0];
+            Res = BinarySearch.Search(arr, Value);
+            Assert.IsNull(Res);
+        }
+
+        [TestMethod]
+        public void TestPairArray()
+        {
+            Value = ExpectedValue = ExistentItem;
+            int[] arr = new int[] { 12, 13, 14, 20, 25, 30, 33, 50, 57, 99 };
+            Res = BinarySearch.Search(Arr, Value);
+            Assert.AreEqual(ExpectedValue, Res);
+        }
+
+        [TestMethod]
+        public void TestOddArray()
+        {
+            Value = 0;
+            int[] arr = new int[] { 12, 13, 14, 20, 25, 30, 33, 50, 57};
+            Res = BinarySearch.Search(Arr, Value);
+            Assert.AreEqual(ExpectedValue, Res);
         }
     }
 }
